0c1b8078509858ccf9efb8ee16246ad380f5c671
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-04-02 18:50:59 -0600

    reorder

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { Form } from './basic/components/Form'
-import UseExample from './basic/components/useExample'
-import UserContextExample from './basic/class/UseContextExample'
-import { use } from 'react'
-import { UserContext } from "./basic/context/User";
+
 
 function App() {
-    const {name, isLogged, updateUser} = use(UserContext);
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Form></Form>
-
-      <UseExample></UseExample>
-      <UserContextExample></UserContextExample>
-      <footer style={{position:"fixed", bottom: 0}}>
-        {
-          isLogged ?
-          <p>Esta logueado</p>
-          : <p>No esta logeado</p>
-        }
-      </footer>
     </>
   )
 }
